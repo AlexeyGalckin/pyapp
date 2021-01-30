4057b20be2b9315bb137b7eb2d7ec5f4b847baf5
Alexey <galckin@localhost.localdomain>
2021-01-30 20:17:54 +0300

    Aplication skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='test_application',
+        version='1.0',
+        description='Description',
+        auto='Alexey Galckin',
+        author_email='AlexeyGalckin@gmail.com',
+        packages['application'],
+        install_requires=[]
+)
